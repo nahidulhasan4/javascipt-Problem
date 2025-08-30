3e9f8379007f6d25041f7394ff48eaaaf97f0838
nahidulhasan4 <nahidhasan4836@gmail.com>
2025-08-30 11:47:10 -0700

    agin js practis

---
+++ basic01/variable.js
@@ -0,0 +1,9 @@
+let  Name = "nahid hasan";
+let FirstName = "nahid";
+let LastName = "Hasan";
+let FullName = FirstName + " " + LastName;
+
+FirstName = "Ayman";
+
+console.log(Name);
+console.log(LastName);
