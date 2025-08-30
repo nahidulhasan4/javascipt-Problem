ba2708d957128aaf810af93f684e1becb23d6ce5
nahidulhasan4 <nahidhasan4836@gmail.com>
2025-08-30 11:52:04 -0700

    here try to remember

--- basic01/variable.js
+++ basic01/variable.js
@@ -1,9 +1,13 @@
 let  Name = "nahid hasan";
 let FirstName = "nahid";
 let LastName = "Hasan";
+
 let FullName = FirstName + " " + LastName;
 
 FirstName = "Ayman";
+let ChangeName = FirstName + " " + LastName;
+
+console.log(ChangeName);
 
 console.log(Name);
-console.log(LastName);
+console.log(FullName);
