98b4c0be2de363c285439c21b9edc3ec70a3c823
nahidulhasan4 <nahidhasan4836@gmail.com>
2025-08-31 11:57:18 -0700

    here add tset code

--- basic01/variable.js
+++ basic01/variable.js
@@ -2,6 +2,8 @@ let  Name = "nahid hasan";
 let FirstName = "nahid";
 let LastName = "Hasan";
 
+
+let MyArry= ['nahid', 'hasan', 123, true, "nurain", 45.5];
 let FullName = FirstName + " " + LastName;
 
 FirstName = "Ayman";
@@ -10,13 +12,13 @@ let ChangeName = FirstName + " " + LastName;
 let AccountEmail = "nahidhasan334gmail.com";
 const AccountPass ='1223nasidji'
 
-let UserDetails = FullName + `<br/>`+ AccountEmail + `<br/>` + AccountPass;
+// let UserDetails = FullName + `<br/>`+ AccountEmail + `<br/>` + AccountPass;
 console.log(
 UserDetails
 );
 
 
-console.log(ChangeName);
+console.log(MyArry);
 
 console.log(Name);
 console.log(FullName);
