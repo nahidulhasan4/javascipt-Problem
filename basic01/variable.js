666eae2aea1c810647234f8f01a8fcf393d23e99
nahidulhasan4 <nahidhasan4836@gmail.com>
2025-09-03 10:15:18 -0700

    here agin start js

--- basic01/variable.js
+++ basic01/variable.js
@@ -1,6 +1,10 @@
 let  Name = "nahid hasan";
 let FirstName = "nahid";
 let LastName = "Hasan";
+let MobileNumber = "01889274145";
+
+let userLogin = true;
+let userLoginNum = Number(userLogin);
 
 
 let MyArry= ['nahid', 'hasan', 123, true, "nurain", 45.5];
@@ -13,12 +17,18 @@ let AccountEmail = "nahidhasan334gmail.com";
 const AccountPass ='1223nasidji'
 
 // let UserDetails = FullName + `<br/>`+ AccountEmail + `<br/>` + AccountPass;
-console.log(
-UserDetails
-);
+// console.log(UserDetails);
 
 
-console.log(MyArry);
+let mobilePhone =  Number(MobileNumber);
+// console.log(MyArry);
 
-console.log(Name);
-console.log(FullName);
+// console.log(Name);
+
+// console.log(mobilePhone);
+console.log(userLoginNum);
+// console.log(userLogin);
+// console.log(typeof MobileNumber);   
+// console.log(typeof mobilePhone);   
+// console.log(typeof userLogin);   
+console.log(typeof userLoginNum);   
