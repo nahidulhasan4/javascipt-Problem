037236e1e1d158a3dc11552e884fd7763793943d
nahidulhasan4 <nahidhasan4836@gmail.com>
2025-09-05 12:17:54 -0700

    here practice switch

--- basic01/if.js
+++ basic01/if.js
@@ -9,31 +9,57 @@ let b = 10;
 // }else{
 //     console.log("this is false");
 // }
-let StudentArry= {
-    name: "nahid hasan",
-    Result: 50,
-    {
-        
-    }
+// let StudentArry= {
+//     name: "nahid hasan",
+//     Result: 50,
     
-}
-console.log(StudentArry.name);
+    
+// }
+// console.log(StudentArry.name);
 
-let Result = 70;
-if (Result >= 80 && Result <=100){
-    console.log("A+");
-}else if(Result >= 70 && Result <80){
-    console.log("A");
-}else if(Result >= 60 && Result <70){
-    console.log("A-");
-}else if(Result >= 50 && Result <60){
-    console.log("B");
-}else if(Result >= 40 && Result <50){
-    console.log("C");
-}else if(Result >= 33 && Result <40){
-    console.log("D");
-}else if(Result >= 0 && Result <33){
-    console.log("Fail");
-}else{
-    console.log("Invalid Result");
+// let Result = 70;
+// if (Result >= 80 && Result <=100){
+//     console.log("A+");
+// }else if(Result >= 70 && Result <80){
+//     console.log("A");
+// }else if(Result >= 60 && Result <70){
+//     console.log("A-");
+// }else if(Result >= 50 && Result <60){
+//     console.log("B");
+// }else if(Result >= 40 && Result <50){
+//     console.log("C");
+// }else if(Result >= 33 && Result <40){
+//     console.log("D");
+// }else if(Result >= 0 && Result <33){
+//     console.log("Fail");
+// }else{
+//     console.log("Invalid Result");
+// }
+let grade = 60;
+
+switch (grade){
+    case(grade >= 80 && grade <=100):
+        console.log("A+");
+        break;
+    case(grade >= 70 && grade <80):
+        console.log("A");
+        break;
+    case(grade >= 60 && grade <70):
+        console.log("A-");
+        break;
+    case(grade >= 50 && grade <60):
+        console.log("B");
+        break;
+    case(grade >= 40 && grade <50):
+        console.log("C");
+        break;
+    case(grade >= 33 && grade <40):
+        console.log("D");
+        break;
+    case(grade >= 0 && grade <33):
+        console.log("Fail");
+        break;
+    default:
+        console.log("Invalid Result");
+
 }
