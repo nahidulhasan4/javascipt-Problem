8b1e86f10637f21b7561fd7586af56b32ab54033
nahidulhasan4 <nahidhasan4836@gmail.com>
2025-09-08 11:38:04 -0700

    here try splice method for add and remove item from arry

---
+++ basic01/arrym.js
@@ -0,0 +1,24 @@
+//   here i will practice js arry and its methodi
+let fruts = ["mango", "banana", "orange", "apple"];
+
+// ARRY JOIN MEHTOD DOING FOR it will be string and it can use selected symol  
+// let fruts2 = fruts.join(" * ");
+
+
+// here splice method use for add or remove arry item
+
+// fruts.splice(2,0,"kiwi","papaya");
+
+// console.log( fruts);
+
+
+// console.log( fruts2);
+// we can find the index using arry indexof method 
+// console.log( fruts.indexOf("banana"));
+
+
+// console.log( fruts.includes("papaya"));
+// console.log( fruts.indexOf("banana"));
+// console.log( fruts.indexOf("banana"));
+// 
+
