b97579e171a2017405808cf813f35caeed804cf3
nahidulhasan4 <nahidhasan4836@gmail.com>
2025-09-09 11:09:53 -0700

    here i practing method

--- basic01/arrym.js
+++ basic01/arrym.js
@@ -3,10 +3,25 @@ let fruts = ["mango", "banana", "orange", "apple"];
 let fruts1 = ["kiwi", "papaya"];
 fruts1.pop();
 fruts1.push("watermelon");
+// we can set arrry length for defind arry how log will be 
+// fruts.length= 2;
+// fruts.shift
 let fruts3 = fruts.concat(fruts1);
 // console.log(fruts3.indexOf("kiwi"));
 
-console.log(fruts1);
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// amra at methiod use kore arry er kono specific index er item ke access korte pari
+let fruit = fruits.at(0);
+// console.log(fruit);
+
+// here we can concert arry to string usieng toString mehtod
+// let myfruts = fruts.toLocaleString()
+// if we use join method it will be more effective than toString method and also you can join arry using you won sign type. 
+let myfruts = fruts.join("*")
+// console.log(myfruts);
+// arrry length use for find how much length have the arry 
+// console.log(fruts.length);
+// console.log(fruts1);
 
 // ARRY JOIN MEHTOD DOING FOR it will be string and it can use selected symol  
 // let fruts2 = fruts.join(" * ");
