4369cf187df9e33d4ca08da6993ddccfb04c8b0f
nahidulhasan4 <nahidhasan4836@gmail.com>
2025-09-13 11:30:10 -0700

    here practice data type also

---
+++ basic01/data.js
@@ -0,0 +1,6 @@
+// akhany js data type niy alochona korbo
+// akta data memory jvaby rakha hoy tar oppor bes kore data k 2 bag 
+// 1. primitive data type
+// 2. non primitive data type
+
+// primitive data type
