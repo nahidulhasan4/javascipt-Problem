6246a3ff2f3bd6756502e813518ab1a605720675
nahidulhasan4 <nahidhasan4836@gmail.com>
2025-09-13 12:03:01 -0700

    here data type memoriseing

--- basic01/data.js
+++ basic01/data.js
@@ -3,4 +3,30 @@
 // 1. primitive data type
 // 2. non primitive data type
 
-// primitive data type
+// primitive data type  
+// 1. string
+example
+let ame = "nahid hasan";
+// string er vitor single quote double quote and backtick use kora jay
+// sring er vitor jodi amra single quote use kori taholy string er vitor double quote use korte parbo and vice versa and also backtick use korte parbo
+// string er maje change korly string er maje change korly seta new string hoye jabe original string ta change hobe na
+// 2. number
+// 3. boolean
+// 4. null
+// 5. undefined
+// 6. symbol
+// 7. bigint
+
+// non primitive data type(reference data type)
+// 1. arry
+// 2. object
+// 3. function
+// 4. date
+// 5. anything else
+
+// non primitive data type reference data type er khetre amra jodi kono variable er value arek variable e assign kori taholy seta copy hoy na seta reference hoy mane original variable er value change korly seta arek variable e reflect korbe
+// let x = 10;
+// let y = x;
+// console.log(x,y);
+// x=20;
+// console.log(x,y);
