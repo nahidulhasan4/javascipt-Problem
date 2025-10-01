ba314ab872c1a98cf285a490853d8a4105f7f685
nahidulhasan4 <nahidhasan4836@gmail.com>
2025-10-01 10:19:46 -0700

    here add fatch filea

---
+++ basic02/fatch.js
@@ -0,0 +1,31 @@
+// here i will practice about fatch api
+
+const url = 'https://jsonplaceholder.typicode.com/posts';
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
+            // fatch api er khetre amra akta promise return kore jeta amra then use kore data access korte pari
+            // then use kore data access korte pari
+            // catch use kore error handle korte pari
+            // fatch api er khetre amra akta promise return kore jeta amra then use kore data access korte pari
+            // then use kore data access korte pari
+            // catch use kore error handle korte pari                                   
+
+            // fatch api er khetre amra akta promise return kore jeta amra then use kore data access korte pari
+            // then use kore data access korte pari
+            // catch use kore error handle korte pari
+            // fatch api er khetre amra akta promise return kore jeta amra then use kore data access korte pari
+            // then use kore data access korte pari
+            // catch use kore error handle korte pari
+
+
+
+            let url = 'https://jsonplaceholder.typicode.com/posts';
+
+            fetch(url)
+                .then(response => response.json())
+                .then(data => console.log(data))
+                .catch(error => console.error('Error:', error));
+                    
