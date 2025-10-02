eea574c3fe79b1e75167a17c2c6ea41e1994177f
nahidulhasan4 <nahidhasan4836@gmail.com>
2025-10-02 11:07:47 -0700

    here have data

--- basic02/ays.js
+++ basic02/ays.js
@@ -28,17 +28,17 @@
 
 // MyFunction();
 
-async function MyFunction() {
-    try {
-        let response = await fetch('https://jsonplaceholder.typicode.com/posts');
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        let data = await response.json();
-        console.log(data);
-    } catch (error) {
-        console.error('There has been a problem with your fetch operation:', error);
-    }
-}
+// async function MyFunction() {
+//     try {
+//         let response = await fetch('https://jsonplaceholder.typicode.com/posts');
+//         if (!response.ok) {
+//             throw new Error('Network response was not ok');
+//         }
+//         let data = await response.json();
+//         console.log(data);
+//     } catch (error) {
+//         console.error('There has been a problem with your fetch operation:', error);
+//     }
+// }
 
-MyFunction();   
+// MyFunction();   
