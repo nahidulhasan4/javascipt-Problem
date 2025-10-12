f04e9b3e07af417913dd720e1fe8753504556efb
nahidulhasan4 <nahidhasan4836@gmail.com>
2025-10-12 11:34:17 -0700

    here add switch case

---
+++ basic02/project.js
@@ -0,0 +1,36 @@
+// akhny akta simple switch case er example
+
+
+let day = 3;
+switch(day){
+    case 1:
+        console.log("sunday");
+        break;
+    case 2:
+        console.log("monday");
+        break;
+    case 3:
+        console.log("tuesday");
+        break;
+    case 4:
+        console.log("wednesday");
+        break;
+    case 5:
+        console.log("thursday");
+        break;
+    case 6:
+        console.log("friday");
+        break;
+    case 7:
+        console.log("saturday");
+        break;
+    default:
+        console.log("invalid day");
+}
+
+// output: tuesday
+
+// ekhane amra switch case er maddhome day er value onujayi day er nam print koreci
+// jodi kono case match na kore tahole default case run hobe and "invalid day" print hobe
+// break statement diye amra case gulo ke alada koreci jate kore ekbar match hole baki case gulo run na kore
+// switch case er value number, string, boolean sob kichu hote pare
