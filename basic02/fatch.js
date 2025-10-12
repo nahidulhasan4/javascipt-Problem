f74149cb445dcdd29eb4dfbd9b6255a69fb3a234
nahidulhasan4 <nahidhasan4836@gmail.com>
2025-10-12 11:35:00 -0700

    here add switch case

--- basic02/fatch.js
+++ basic02/fatch.js
@@ -28,3 +28,8 @@ fetch(url)
                 .then(response => response.json())
                 .then(data => console.log(data))
                 .catch(error => console.error('Error:', error));
+// fatch api er khetre amra akta promise return kore jeta amra then use kore data access korte pari
+// then use kore data access korte pari
+// catch use kore error handle korte pari
+// fatch api er khetre amra akta promise return kore jeta amra then use kore data access korte pari
+// then use kore data access korte pari 
