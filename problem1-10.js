385e2c35c90a319b78b8f37e5691ed4e92fea0db
nahidulhasan4 <nahidhasan4836@gmail.com>
2025-10-23 11:31:16 -0700

    here add new problem for find name index

--- problem1-10.js
+++ problem1-10.js
@@ -169,7 +169,7 @@ let uniqueNumbersthree = numbers.reduce((acc, curr) => {
   return acc.includes(curr) ? acc : [...acc, curr];
 }, []);
 
-// 
+// problem number e10 
 
 const descrip ="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam possimus earum cupiditate eveniet rerum nisi consequatur reprehenderit nam esse quo! Quam temporibus, modi facilis quia, magni assumenda dolorum esse sit tenetur quas nisi ea, rem veniam ipsum. Ducimus culpa vm accusamus tempore perspiciatis beatae officiis, neque inventore laudantium dolor sed? "
     
